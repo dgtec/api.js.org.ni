db9a20f43f7a389f581acf1184eae61a475a92bc
Hosmel Quintana <hosmelq@gmail.com>
2017-08-30 21:42:08 -0600

    add micro-cors package

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const {microGraphiql, microGraphql} = require('graphql-server-micro');
 const {send} = require('micro');
+const cors = require('micro-cors')();
 const {get, post, router} = require('microrouter');
 
 const schema = require('./schema');
@@ -7,9 +8,9 @@ const schema = require('./schema');
 const graphqlHandler = microGraphql({schema});
 const graphiqlHandler = microGraphiql({endpointURL: '/graphql'});
 
-module.exports = router(
+module.exports = cors(router(
   get('/graphiql', graphiqlHandler),
   get('/graphql', graphqlHandler),
   post('/graphql', graphqlHandler),
   (req, res) => send(res, 404, 'not found'),
-);
+));
